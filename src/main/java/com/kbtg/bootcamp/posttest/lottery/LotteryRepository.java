6aa21b8c6cc6b49ac674e66bea6452557fac50ee
pongsakon promma <pongsakonpromma@gmail.com>
2024-02-24 02:17:10 +0700

    feat : complete Story: EXP03

--- src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryRepository.java
+++ src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryRepository.java
@@ -3,8 +3,10 @@ package com.kbtg.bootcamp.posttest.lottery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LotteryRepository extends JpaRepository<Lottery, Long> {
 
-
+    Optional<Lottery> findByTicketId(String ticketId);
 }
