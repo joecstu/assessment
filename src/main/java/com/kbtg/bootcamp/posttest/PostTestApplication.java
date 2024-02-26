72680e9b167e77b3e27eebac772963f745015520
pongsakon promma <pongsakonpromma@gmail.com>
2024-02-26 22:25:26 +0700

    chore : edit format code

--- src/main/java/com/kbtg/bootcamp/posttest/PostTestApplication.java
+++ src/main/java/com/kbtg/bootcamp/posttest/PostTestApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class PostTestApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(PostTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(PostTestApplication.class, args);
+    }
 
 }
